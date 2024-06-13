14957862a06d14fb242707cf11a2c93086936142
iamDiegoOliveira <diegods1505@gmail.com>
2024-06-13 10:03:19 -0300

    Pequenos testes básicos

---
+++ conta-banco/src/Alunos.java
@@ -0,0 +1,21 @@
+public class Alunos {
+    public static void main(String[] args) {
+       
+        int i = 0;
+
+        while (i<= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+       
+
+
+
+
+
+
+        
+        }
+    }
+
